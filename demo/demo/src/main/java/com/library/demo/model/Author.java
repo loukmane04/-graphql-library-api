add6a155b4d80bcf79660ce32ded63acc0cf4f70
aymene-bd <aymenebouderka@gmail.com>
2025-12-28 21:22:21 +0100

    Add Author, Book, and Category entities with JPA relationships

--- demo/demo/src/main/java/com/library/demo/model/Author.java
+++ demo/demo/src/main/java/com/library/demo/model/Author.java
@@ -1,9 +1,31 @@
 package com.library.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
-	public Author() {
-		// TODO Auto-generated constructor stub
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idA;
 
+    @Column(nullable = false)
+    private String name;
+
+    private Integer age;
+
+    private String nationality;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
 }
--- demo/demo/src/main/java/com/library/demo/model/Category.java
+++ demo/demo/src/main/java/com/library/demo/model/Category.java
@@ -1,9 +1,34 @@
 package com.library.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
-	public Category() {
-		// TODO Auto-generated constructor stub
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idC;
 
+    @Column(nullable = false)
+    private String categoryName;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory")
+    private List<Category> subCategories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "category")
+    private List<Book> books = new ArrayList<>();
 }
